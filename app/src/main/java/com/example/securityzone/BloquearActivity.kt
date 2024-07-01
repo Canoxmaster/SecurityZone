f8b15d2d70066d190089a4783731ef84333df378
AndresCanto <62972472+AndresCanto@users.noreply.github.com>
2024-07-01 13:45:14 -0600

    UI pantallas principales

---
+++ app/src/main/java/com/example/securityzone/BloquearActivity.kt
@@ -0,0 +1,20 @@
+package com.example.securityzone
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BloquearActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bloquear)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
