eaedb2cedd9dfcc11975d696caf8cba43c16e60c
Daniel <Danielluma242004@hotmail.com>
2024-07-03 09:33:16 -0600

    Back Buttons Update

--- app/src/main/java/com/example/securityzone/MotivoActivity.kt
+++ app/src/main/java/com/example/securityzone/MotivoActivity.kt
@@ -1,6 +1,8 @@
 package com.example.securityzone
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MotivoActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        setupButtonClickListeners()
+    }
+    private fun setupButtonClickListeners() {
+        findViewById<ImageButton>(R.id.backButton).setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
