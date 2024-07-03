c293035d7e501a5167be4bd8b6e0757f22675a00
Daniel <Danielluma242004@hotmail.com>
2024-07-03 12:06:39 -0600

    Status Button Update

--- app/src/main/java/com/example/securityzone/MainActivity.kt
+++ app/src/main/java/com/example/securityzone/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.securityzone
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var isBlocked = true
+    private var spanish = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,6 +25,12 @@ class MainActivity : AppCompatActivity() {
 
         // Set up click listeners for each button
         setupButtonClickListeners()
+
+        // Set up the toggle for the TextView
+        setupTextViewToggle()
+
+        // Set up the toggle for the country
+        setupCountryToggle()
     }
 
     private fun setupButtonClickListeners() {
@@ -38,6 +48,39 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<ImageButton>(R.id.informeBtn).setOnClickListener {
             startActivity(Intent(this, InformeActivity::class.java))
+        }
+    }
+    private fun setupTextViewToggle() {
+        val statusTextView = findViewById<TextView>(R.id.textView2)
+        statusTextView.setOnClickListener {
+            if (isBlocked) {
+                statusTextView.text = "Status: Abierto"
+                statusTextView.setBackgroundColor(resources.getColor(R.color.green, theme))
+                isBlocked = false
+            } else {
+                statusTextView.text = "Status: Bloqueado"
+                statusTextView.setBackgroundColor(resources.getColor(R.color.black, theme))
+                isBlocked = true
+            }
+        }
+    }
+    private fun setupCountryToggle() {
+        val toggleButton = findViewById<ToggleButton>(R.id.toggleIdioma)
+
+        // Set the initial state
+        updateToggleButtonBackground(toggleButton, spanish)
+
+        toggleButton.setOnClickListener {
+            spanish = !spanish
+            updateToggleButtonBackground(toggleButton, spanish)
+        }
+    }
+
+    private fun updateToggleButtonBackground(button: ToggleButton, isSpanish: Boolean) {
+        if (isSpanish) {
+            button.setBackgroundResource(R.drawable.espana) // Change this to your actual drawable resource for "on"
+        } else {
+            button.setBackgroundResource(R.drawable.usa) // Change this to your actual drawable resource for "off"
         }
     }
 }
